1999adcd5211a1b30400abec4bb6aaa810f895d3
Andrew Xue <a3xue@edu.uwaterloo.ca>
2017-04-03 15:08:42 -0400

    Add files via upload

--- PewPewPew.java
+++ PewPewPew.java
@@ -2,12 +2,16 @@ package PewPewPew;
 
 //Made by Andrew Xue
 //a3xue@edu.uwaterloo.ca
-//Bullethell game.
+//Shoot things trying to shoot you. Currently more of a framework for a working model. Enemies, pathing, and many characteristics about the enemy
+//   and the player can be changed. Includes a working model of movement, combat and taking damage.
 //NO KNOWN BUGS ATM.
 
 //UNIMPLEMENTED:
 //  Dying.
 //  Explosion graphics.
+//  Need more ships
+//  Power-ups
+//  Background
 
 import java.awt.Color;
 import java.awt.Font;
@@ -18,7 +22,6 @@ import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
-
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -35,6 +38,20 @@ public class PewPewPew {
 	ArrayList<enemybullet> enemybullets = new ArrayList<enemybullet>();
 	ArrayList<playerbullet> playerbullets = new ArrayList<playerbullet>();
 	
+	// List of different spawn paths and characteristics
+	ArrayList<spawn> spawnlst = new ArrayList<spawn>();
+	int [] templst = {712,921,660,906,588,884,498,854,416,820,341,783,286,756,220,718,185,692,161,660,147,620,151,579,186,532,267,486,347,456,445,427,522,406,608,382,674,361,745,324,780,287,796,253,793,204,777,177,705,148,605,118,532,101,441,90,348,89,283,88,237,86,188,81,152,75,116,69,83,46};
+	
+	int [][] pathmatrix = {{1,2},{3,4}};
+	
+	// Variables for spawn logic.
+	int spawned=0;  //Counts the number of enemies spawned
+	int spawnpattern=0;   //Controls the spawn pattern path that the enemies follow
+	// Controls the cooldown of spawning enemies
+	int spawnwait=0;
+	
+	//(int [] tempcurvee, boolean tempmirrored, int tempnumenemy, int tempspawncool)
+	
 	// Array of colours to be picked randomly for enemy bullets
 	Color[] colourlst={Color.RED,Color.BLUE,Color.GREEN,Color.ORANGE,Color.CYAN};
 	Random colourpick = new Random();
@@ -42,15 +59,14 @@ public class PewPewPew {
 	// To be used for enemy spawns at random positions
 	Random spawnpick = new Random();
 	
-	// To be used for random pews
+	/*// To be used for random pews
 	Random pewpick = new Random();
 	int pewx;
 	// pewy used for placing pews above or below the ship
 	Random pewypick = new Random();
-	int pewy;
+	int pewy;*/
 	
 	// Integers controlling the timing of enemy spawns
-	int spawncooldown=300;
 	int spawncount=0;
 	
 	// Function for creating a path that goes straight up and down
@@ -60,9 +76,10 @@ public class PewPewPew {
 	}
 	
 	// Basic curved path
-	int [] curve = {934, 27, 862, 76, 837, 109, 822, 137, 805, 175, 794, 212, 776, 277, 763, 361, 758, 412, 758, 467, 763, 517, 770, 562, 783, 605, 802, 653, 817, 698, 827, 732, 836, 763, 847, 799, 868, 845, 886, 879, 905, 910, 915, 925};
-
-	
+	int [] curvee = 
+		{983,890,964,878,947,864,919,835,872,786,827,730,797,667,776,607,768,528,784,427,804,355,823,286,855,215,898,150,941,107};
+	int [] mirrore = 
+		{17,890,36,878,53,864,81,835,128,786,173,730,203,667,224,607,232,528,216,427,196,355,177,286,145,215,102,150,59,107};
 	// Detects collision between two objects
 	boolean collision(int x1, int y1, int x2, int y2,int siz1, int siz2){
 		if (x1+siz1>=x2&&x1<=x2+siz2&&y1+siz1>=y2&&y1<=y2+siz2){
@@ -71,7 +88,23 @@ public class PewPewPew {
 		return false;
 	}
 	
-	// Highest class, only stores the x and ycoordinates of the object, the size, and whether it is out of the game screen
+	
+	private class spawn{
+		boolean mirrored = true;
+		int [] curve;
+		int [] mirror;
+		int numenemy;
+		int spawncool;
+		private spawn(int [] tempcurve, int [] tempmirror, boolean tempmirrored, int tempnumenemy, int tempspawncool){
+			curve=tempcurve;
+			mirror=tempmirror;
+			mirrored=tempmirrored;
+			numenemy=tempnumenemy;
+			spawncool=tempspawncool;
+		}
+	}
+	
+	// Highest class, stores the x and ycoordinates of the object, the size, and whether it is out of the game screen
 	private class entity{
 		double x,y;
 		boolean remove=false;
@@ -87,8 +120,8 @@ public class PewPewPew {
 	//   shots), count (to be used in conjunction with bullet cooldown time), health of the ship, speed of the ship, damage of the ship's
 	//   bullets, speed of the ship's bullets.
 	private class ship extends entity{
-		int bullcool,count,health,speed,bulletdamage,bulletspeed;
-		ship(double tempx,double tempy,int tempsize,int tempbullcool,int temphealth,int tempspeed,int tempbulletdamage,int tempbulletspeed){
+		int bullcool,count,health,speed,bulletdamage,bulletspeed,bulletsize,maxhealth;
+		ship(double tempx,double tempy,int tempsize,int tempbullcool,int temphealth,int tempspeed,int tempbulletdamage,int tempbulletspeed,int tempbulletsize,int tempmaxhealth){
 			super(tempx,tempy,tempsize);
 			bullcool=tempbullcool;
 			count=0;
@@ -96,11 +129,13 @@ public class PewPewPew {
 			speed=tempspeed;
 			bulletdamage=tempbulletdamage;
 			bulletspeed=tempbulletspeed;
+			bulletsize=tempbulletsize;
+			maxhealth=tempmaxhealth;
 		}
 	}
 	
-	// Subclass for the player.
 	
+	// Subclass for the player. Includes an image to be used as a sprite for the player
 	private class playership extends ship{
 		// These two variables used for following the ship.
 		private ImageIcon temp = new ImageIcon("Untitled.png");
@@ -110,7 +145,7 @@ public class PewPewPew {
 		double angletomouse;
 		private playership(){
 			// Initializes superclass components
-			super(100,900,20,40,100,4,10,15);
+			super(100,900,20,40,100,4,5,20,5,100);
 		}
 		void shipmove(){
 			// If the mouse is in the ship, no movement
@@ -125,8 +160,8 @@ public class PewPewPew {
 			// If the bullet is not on cooldown, shoot
 			if (count==bullcool){
 				// Create one playerbullet on the left of the ship, and one to the right, using properties of the Ship superclass
-				playerbullets.add(new playerbullet(x,y-5,5,bulletdamage,bulletspeed));
-				playerbullets.add(new playerbullet(x+15,y-5,5,bulletdamage,bulletspeed));
+				playerbullets.add(new playerbullet(x,y-5,bulletsize,bulletdamage,bulletspeed));
+				playerbullets.add(new playerbullet(x+15,y-5,bulletsize,bulletdamage,bulletspeed));
 				count=0;}
 			else{
 				// Reduce the bullet cooldown time
@@ -137,6 +172,7 @@ public class PewPewPew {
 		public void die() {
 		}
 	}
+	
 	
 	// Subclass of entity, player's bullets. Has properties of the bullet's damage, speed and size
 	private class playerbullet extends entity{
@@ -170,6 +206,7 @@ public class PewPewPew {
 		}
 	}
 	
+	
 	// Subclass of the ship class, with two extra elements, the index, angle and pathing array. These are all used to move the ship.
 	private class enemy extends ship{
 		int index;
@@ -178,8 +215,8 @@ public class PewPewPew {
 		int pathing[];
 		private ImageIcon tempimage = new ImageIcon("enemy.png");
 		private Image pic = tempimage.getImage();
-		private enemy(int x,int y,int hea,int siz,int spe,int bulletspeedtemp,int pathig[],int bulletdamagetemp,int bullcooltemp){
-			super(x,y,siz,bullcooltemp,hea,spe,bulletdamagetemp,bulletspeedtemp);
+		private enemy(int x,int y,int hea,int siz,int spe,int bulletspeedtemp,int pathig[],int bulletdamagetemp,int bullcooltemp,int tempbulletsize){
+			super(x,y,siz,bullcooltemp,hea,spe,bulletdamagetemp,bulletspeedtemp,tempbulletsize,hea);
 			pathing=pathig;
 			index=pathig.length-1;
 			bulletspeed=bulletspeedtemp;
@@ -191,6 +228,8 @@ public class PewPewPew {
 		//   point to the point in the pathing array, and moves in that direction by its speed.
 		// The pathing array can be created in the pathing.java file by simply drawing. Custom patterns can be implemented.
 		private void move(){
+			x+= Math.cos(angle)*speed;
+			y+= Math.sin(angle)*speed;
 			if (index>=1){
 				double xdist = (x-pathing[index-1])*(x-pathing[index-1]);
 				double ydist = (y-pathing[index])*(y-pathing[index]);
@@ -198,10 +237,8 @@ public class PewPewPew {
 				//   calculating its next angle of travel.
 				if (xdist+ydist<=30){
 					index-=2;
-					if (index>=1){angle=Math.atan2(pathing[index]-(double)y, pathing[index-1]-(double)x);}}
-				x+= Math.cos(angle)*speed;
-				y+= Math.sin(angle)*speed;}
-			else{
+					if (index>=1){angle=Math.atan2(pathing[index]-(double)y, pathing[index-1]-(double)x);}}}
+			else if (x>1000||x+size<0||y>1000||y-size<0){
 				// If it has finished its path, it is removed
 				remove=true;
 			}
@@ -213,7 +250,7 @@ public class PewPewPew {
 			if (count==bullcool){
 			// Takes the angle between the ship and the player's ship, and creates a bullet moving in that direction with the bullet's damage
 			//   and speed taken from the superclass ship. Also randomizes their colour.
-			enemybullets.add(new enemybullet(x+size/2,y+size/2,bulletspeed,bulletdamage,15,colourlst[colourpick.nextInt(4)],
+			enemybullets.add(new enemybullet(x+size/2-bulletsize/2,y+size/2-bulletsize/2,bulletspeed,bulletdamage,bulletsize,colourlst[colourpick.nextInt(4)],
 					Math.atan2(player.y+player.size/2-(double)(y+size/2), player.x+player.size/2-(double)(x+size/2))));
 			count=0;}
 			else{
@@ -270,6 +307,12 @@ public class PewPewPew {
 		window.setResizable(false);
 		window.add(new PewGrid());
 		window.addMouseMotionListener(new mouseevent());
+		
+		spawnlst.add(new spawn(templst,null,false,5,100));
+		int [] t = {934,941,915,921,886,892,856,860,790,800,736,742,694,685,647,635,585,570,505,480,430,401,350,320,282,251,206,178,113,86};
+		int [] l = {66,941,85,921,114,892,144,860,210,800,264,742,306,685,353,635,415,570,495,480,570,401,650,320,718,251,794,178,887,86};
+		spawnlst.add(new spawn(t,l,true,5,100));
+		
 		move();
 	}
 	
@@ -278,13 +321,29 @@ public class PewPewPew {
 				//(int x,int y,int hea,int siz,int spe,int bulletspeedtemp,int pathig[],int bulletdamagetemp,int bullcooltemp)
 		while (true){
 			// Spawn enemies at set intervals
-			if (spawncount==spawncooldown){
-				// Create an enemy that follows a preset path
-				enemylst.add(new enemy(curve[0],curve[1],100,50,1,2,curve,25,100));
-				spawncount=0;
+			if (spawnwait==0){
+				if (spawncount==spawnlst.get(spawnpattern).spawncool){
+					//int x,int y,int hea,int siz,int spe,int bulletspeedtemp,int pathig[],int bulletdamagetemp,int bullcooltemp,int tempbulletsize
+					// Create an enemy that follows a preset path
+					enemylst.add(new enemy(spawnlst.get(spawnpattern).curve[spawnlst.get(spawnpattern).curve.length-2],spawnlst.get(spawnpattern).curve[spawnlst.get(spawnpattern).curve.length-1],50,50,1,2,spawnlst.get(spawnpattern).curve,25,300,15));
+					if (spawnlst.get(spawnpattern).mirrored){
+						enemylst.add(new enemy(spawnlst.get(spawnpattern).mirror[spawnlst.get(spawnpattern).mirror.length-2],spawnlst.get(spawnpattern).mirror[spawnlst.get(spawnpattern).mirror.length-1],50,50,1,2,spawnlst.get(spawnpattern).mirror,25,300,15));
+					}
+					spawned++;
+					spawncount=0;
+					if (spawned==spawnlst.get(spawnpattern).numenemy){
+						spawnpattern = spawnpick.nextInt(spawnlst.size());
+						spawncount=0;
+						spawned=0;
+						spawnwait=1000;
+					}
+				}
+				else{
+					spawncount++;
+				}
 			}
 			else{
-				spawncount++;
+				spawnwait--;
 			}
 			window.repaint();
 			// Set frame rate
@@ -294,6 +353,7 @@ public class PewPewPew {
 			}
 		}
 	}
+	
 	
 	// JComponent with all the game elements
 	private class PewGrid extends JComponent {
@@ -354,7 +414,7 @@ public class PewPewPew {
 					grap.setColor(Color.RED);
 					grap.fillRect((int)enemylst.get(i).x, (int)enemylst.get(i).y-15, (int)enemylst.get(i).size, 5);
 					grap.setColor(Color.GREEN);
-					grap.fillRect((int)enemylst.get(i).x, (int)enemylst.get(i).y-15, (int)(enemylst.get(i).size*((float)enemylst.get(i).health/100)), 5);
+					grap.fillRect((int)enemylst.get(i).x, (int)enemylst.get(i).y-15, (int)(enemylst.get(i).size*((float)enemylst.get(i).health/enemylst.get(i).maxhealth)), 5);
 				}				
 			}
 		}
--- pathing.java
+++ pathing.java
@@ -1,8 +1,10 @@
-package pathing;
+package PewPewPew;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
@@ -21,7 +23,9 @@ public class pathing {
 	int count=0;
 	int cooldown=10;
 	int index=0;
+	boolean done = false;
 	ArrayList<Integer> pathi = new ArrayList<Integer>();
+	ArrayList<Integer> mirrorpathi = new ArrayList<Integer>();
 	public static void main(String[] args) {
 		new pathing().go();
 	}
@@ -33,16 +37,56 @@ public class pathing {
 		window.setResizable(false);
 		window.add(new pathigrid());
 		window.addMouseMotionListener(new mouseevent());
+		window.addKeyListener(new keyevent());
 		goe();
 	}
 	
+	private void printlst(){
+		if (pathi.size()==1){
+			System.out.println("{"+pathi.get(0)+"};");
+		}
+		else{
+			System.out.print("{");
+			for (int i=pathi.size()/2-1;i>=1;i--){
+			System.out.print(pathi.get(2*i)+","+pathi.get(2*i+1)+",");
+			}
+			System.out.print(pathi.get(0)+","+pathi.get(1)+"};");
+		}
+		System.out.println();
+		if (mirrorpathi.size()==1){
+			System.out.println("{"+mirrorpathi.get(0)+"};");
+		}
+		else{
+			System.out.print("{");
+			for (int i=mirrorpathi.size()/2-1;i>=1;i--){
+			System.out.print(mirrorpathi.get(2*i)+","+mirrorpathi.get(2*i+1)+",");
+			}
+			System.out.print(mirrorpathi.get(0)+","+mirrorpathi.get(1)+"};");
+		}
+		
+	}
+	
 	private void goe(){
-		while (true){
-			System.out.println(pathi);
+		while (!done){
 			try{Thread.sleep(50);}
 			catch(Exception ex){}
 			window.repaint();
 		}
+		printlst();
+	}
+	
+	private class keyevent implements KeyListener{
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode()==KeyEvent.VK_E){
+				done = true;
+			}
+			
+		}
+		public void keyReleased(KeyEvent e) {}
+		public void keyTyped(KeyEvent e) {}
+		
 	}
 	
 	private class pathigrid extends JComponent {
@@ -62,6 +106,8 @@ public class pathing {
 			if (count==cooldown){
 			pathi.add(e.getX()-5);
 			pathi.add(e.getY()-35);
+			mirrorpathi.add(1000-e.getX()+5);
+			mirrorpathi.add(e.getY()-35);
 			count=0;}
 			else{
 				count++;
